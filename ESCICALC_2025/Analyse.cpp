cd60a7f27c9435895e6293c25dccfb3a5d05133f
BillyJo12 <Josephmav12@gmail.com>
2024-12-10 13:09:53 +0100

    V.0

---
+++ ESCICALC_2025/Analyse.cpp
@@ -0,0 +1,9 @@
+#include "Analyse.h"
+Console.Writeline("ESCICalc 2025");
+Console.Writeline("Copyright (c) 2024 Promotion BFSDA 2027");
+
+
+Console.Writeline("Veuillez entrer l'expression Mathematique : ");
+string ? expression = Console.Readline();
+
+Console.Writeline(Voici L'exprssion pour laquelle je dois faire le calcul ')
