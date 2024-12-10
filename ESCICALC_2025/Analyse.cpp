5f058bf9e91d6113ed11f4d02f65ebc440a41604
BillyJo12 <Josephmav12@gmail.com>
2024-12-10 13:13:07 +0100

    V0

--- ESCICALC_2025/Analyse.cpp
+++ ESCICALC_2025/Analyse.cpp
@@ -6,4 +6,4 @@ Console.Writeline("Copyright (c) 2024 Promotion BFSDA 2027");
 Console.Writeline("Veuillez entrer l'expression Mathematique : ");
 string ? expression = Console.Readline();
 
-Console.Writeline(Voici L'exprssion pour laquelle je dois faire le calcul ')
+Console.Writeline(Voici L'exprssion pour laquelle je dois faire le calcul {e ')
